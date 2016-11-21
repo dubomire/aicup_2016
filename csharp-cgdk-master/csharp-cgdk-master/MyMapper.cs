73dae6525505d76203b9e6162fb74dea619dda56
Dubomire <dubomire@gmail.com>
2016-11-21 15:51:44 +0300

    Доработан класс карты при построении базовых точек

--- csharp-cgdk-master/csharp-cgdk-master/MyMapper.cs
+++ csharp-cgdk-master/csharp-cgdk-master/MyMapper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk.Model;
@@ -9,6 +10,8 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
 {
     class MyMap
     {
+        public enum PointType { Tree, Wizard, Tower, Base, Allies, Enemy, Waypoint}
+        
         public MyMap(Wizard self, World world, Game game)
         {
             this.self = self;
@@ -27,13 +30,29 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
 
         private void InitBaseWaypoints()
         {
-            mapsize = game.MapSize;
+            MapPoint MyBase = GetBasePoint();
+            waypoints.AddPoint(100.0,0,PointType.Waypoint);
+            waypoints.AddPoint(0,0,PointType.Waypoint);
+            waypoints.AddPoint(0,0,PointType.Waypoint);
+            waypoints.AddPoint(0,0,PointType.Waypoint);
+            waypoints.AddPoint(0,0,PointType.Waypoint);
+        }
+
+        private MapPoint GetBasePoint()
+        {
+            for (int i = 0; i < this.world.Buildings.Count(); i++)
+            {
+                if (this.world.Buildings[i].Faction == self.Faction) && (this.world.Buildings[i].Type=BuildingType.FactionBase)
+                {
+                    return new MapPoint(this.world.Buildings[i].X,this.world.Buildings[i].Y, PointType.Base)
+                }
+            }
+
         }
 
         private Wizard self;
         private  World world;
         private  Game game;
-        private double mapsize;
 
         private PointList waypoints;
         private PointList buildings;
@@ -46,13 +65,21 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
         {
         
         }
+
+        public void AddPoint(int x, int y, MyMap.PointType ptype)
+        {
+             this.Add(new MapPoint(x,y,ptype));
+        }
     }
 
     class MapPoint
     {
-        public enum PointType { Tree, Wizard, Tower, Base, Allies, Enemy, Waypoint}
 
-      public MapPoint(int x, int y, PointType pType)
+        public MapPoint()
+        {
+        }
+
+        public MapPoint(int x, int y, MyMap.PointType pType)
       {
           this.pType = pType;
           this.x = x;
@@ -61,8 +88,8 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
 
         private int x;
         private int y;
-        private PointType pType;
-        public PointType pointType => pType;
+        private MyMap.PointType pType;
+        public MyMap.PointType pointType => pType;
 
 
     }
